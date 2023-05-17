7fac993ad6a0c0ceab5dafd87ef765997f72b65b
Renan <renan.fachin@hotmail.com>
2023-05-17 15:51:25 -0300

    Habilitando o cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+// Habilitando o cors para que seja possível um front end acessar as infos do back end
+app.register(cors, {
+  origin: true, // todas URLS de front-end podem acessar o back-end
+})
 
 // Rotas da aplicação
 app.register(memoriesRoutes)
